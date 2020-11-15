fbda8d7b4408a243601d919289041b1341a84aa8
Junlin Zhu <junlin.zhu@plus.ai>
2020-11-15 22:05:17 +0800

    feat(webpack): SyncWaterFallHook SyncLoopHook

--- Webpack/Tapable-Test/src/Car.js
+++ Webpack/Tapable-Test/src/Car.js
@@ -1,12 +1,16 @@
 const {
   SyncHook,
   SyncBailHook,
+  SyncWaterfallHook,
+  SyncLoopHook,
 } = require('tapable');
 
 module.exports = class Car {
   constructor() {
     this.brakeHook = new SyncBailHook();
+    this.accelerateHook = new SyncWaterfallHook(['Speed']);
     this.startHook = new SyncHook(['Speed']);
+    this.loopStartHook = new SyncLoopHook();
   }
 
   start(speed) {
@@ -15,5 +19,13 @@ module.exports = class Car {
 
   brake() {
     this.brakeHook.call();
+  }
+
+  accelerate() {
+    this.accelerateHook.call();
+  }
+
+  loopStart() {
+    this.loopStartHook.call();
   }
 }
--- Webpack/Tapable-Test/src/index.js
+++ Webpack/Tapable-Test/src/index.js
@@ -2,12 +2,12 @@ const Car = require('./Car');
 
 const car = new Car();
 
-// Base Case
-car.startHook.tap('StartPlugIn', () => {
-  console.log('启动')
-})
+// SyncHook
+// car.startHook.tap('StartPlugIn', () => {
+//   console.log('启动')
+// })
 
-// 像Plugin传递参数
+// 
 car.startHook.tap('startPluginWithPara', (speed) => {
   console.log('Start Speed', speed)
 });
@@ -24,8 +24,6 @@ car.brakeHook.tap('brakePlugin3', () => {
   console.log('brake3');
 })
 
-// brake1, brake2, brake3
-
 // SyncBailHook Break
 car.brakeHook.tap('brakePlugin2', () => {
   console.log('brake1');
@@ -40,3 +38,40 @@ car.brakeHook.tap('brakePlugin2', () => {
 // brake1, brake2
 
 car.brake();
+
+car.accelerateHook.tap('acceleratePlugin1', (speed1) => {
+  console.log(speed1);
+
+  return speed1 + 50;
+});
+
+car.accelerateHook.tap('acceleratePlugin2', (speed2) => {
+  console.log(speed2);
+
+  return speed2 + 50;
+});
+
+car.accelerateHook.tap('acceleratePlugin3', (speed3) => {
+  console.log(speed3);
+
+  return speed3;
+});
+
+car.accelerateHook.call(50);
+
+let index = 0;
+
+car.loopStartHook.tap('startPlugin1', () => {
+  if (index < 5) {
+    console.log(`启动`);
+
+    index++;
+    return 1;
+  }
+});
+
+car.loopStartHook.tap('startPlugin2', () => {
+  console.log(`启动成功`);
+});
+
+car.loopStart();
